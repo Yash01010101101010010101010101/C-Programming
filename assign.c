c3570460b9b9d12eb7cfba399f8fbd10084e70b8
GitHub User <github@example.com>
2026-09-10 11:00:00 +0530

    Add assignment exercises

---
+++ assign.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main() {
+    char r,c;
+    for(r=1;r<6;r++)
+     { printf("\n");
+        for(c=1;c<6;c++){
+            printf("*");
+        }
+    }
+printf("\n");
+return 0;
+
+
+
+    }
+
+
+
+
+
+
+
+
